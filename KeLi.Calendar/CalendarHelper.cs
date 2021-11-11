88b2f091405cdd775045536c4e7ea39983f0dd38
KeLi <kelicto@protonmail.com>
2021-11-11 23:16:43 +0800

    Updates code.

---
+++ KeLi.Calendar/CalendarHelper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading;
+
+using static System.DateTime;
+
+namespace KeLi.Calendar
+{
+    public class CalendarHelper
+    {
+        private static int _year;
+
+        private static int _month;
+
+        private static int _height;
+
+        public static void ShowCalendar()
+        {
+            ShowCurrentMonth(true);
+
+            SearchAnyMonth();
+
+            Thread.Sleep(3000);
+
+            for (var i = 0; i < 100000; i++)
+            {
+                Console.Clear();
+
+                ShowCurrentMonth();
+
+                Console.WriteLine();
+
+                ShowAnyMonth();
+
+                var seek = new Random((int)(Now.Ticks & 0xffffffffL) | (int)(Now.Ticks >> 32)).Next(10) * 100;
+
+                Thread.Sleep(seek);
+            }
+        }
+
+        private static void SearchAnyMonth()
+        {
+            while (true)
+            {
+                Console.SetCursorPosition(0, CalendarUtil.ComputeCurrentMonthLine());
+
+                Console.Write("Inputs Year[1-9999]: ");
+
+                _year = Convert.ToInt32(Console.ReadLine());
+
+                if (_year > 0 && _year < 9999)
+                    break;
+            }
+
+            while (true)
+            {
+                Console.SetCursorPosition(0, CalendarUtil.ComputeCurrentMonthLine() + 1);
+
+                Console.Write("Inputs Month[1-12]: ");
+
+                _month = Convert.ToInt32(Console.ReadLine());
+
+                if (_month > 0 && _month < 13)
+                    break;
+            }
+
+            Console.WriteLine();
+
+            _height = CalendarUtil.ComputeCurrentMonthLine() + CalendarUtil.ComputeAnyMonthLine(_year, _month) + 2;
+
+            // Updates console's window height.
+            Console.WindowHeight = _height;
+
+            // Updates console's buffer height.
+            Console.BufferHeight = _height;
+
+            CalendarUtil.ShowAnyMonth(_year, _month, true);
+        }
+
+        private static void ShowCurrentMonth(bool flag = false)
+        {
+            CalendarUtil.ShowCurrentMonth(flag);
+        }
+
+        private static void ShowAnyMonth()
+        {
+            CalendarUtil.ShowAnyMonth(_year, _month, false, _height);
+        }
+    }
+}
---
+++ KeLi.Calendar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Media;
+
+using KeLi.Calendar.Properties;
+
+namespace KeLi.Calendar
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Title = "Calendar";
+            Console.WindowWidth = 26;
+            Console.WindowHeight = 29;
+            Console.BufferWidth = 26;
+            Console.BufferHeight = 29;
+
+            try
+            {
+                new SoundPlayer(Resources.SanShao).Play();
+                CalendarHelper.ShowCalendar();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
